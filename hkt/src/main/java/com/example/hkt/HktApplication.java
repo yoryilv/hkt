b8c6cc59aa5bf72def3acf03ccd2fa6c4d0c6046
jorge l <jorge.leandro@utec.edu.pe>
2024-05-21 14:19:31 -0500

    iiii

---
+++ hkt/src/main/java/com/example/hkt/HktApplication.java
@@ -0,0 +1,13 @@
+package com.example.hkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HktApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HktApplication.class, args);
+	}
+
+}
